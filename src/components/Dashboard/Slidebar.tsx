95f26a1714c02ccbf1be7fb87d8810e3c62b6631
danipoav <dapoav2002@gmail.com>
2025-02-01 15:07:19 +0100

    Slide bar animation

--- src/components/Dashboard/Slidebar.tsx
+++ src/components/Dashboard/Slidebar.tsx
@@ -17,28 +17,33 @@ export default function SlideBar() {
 
             <OptionList>
                 <Link to="/home" style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <OptionContainer active={location.pathname === '/home'}>
+                    <OptionContainer active={location.pathname === '/home'} >
                         <svg xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" viewBox="0 0 24 24"><path fill="currentColor" d="M2 2h9v7H2zm2 2v3h5V4zm9-2h9v11h-9zm2 2v7h5V4zM2 11h9v11H2zm2 2v7h5v-7zm9 2h9v7h-9zm2 2v3h5v-3z" /></svg>
+                        <span className="hover-text">overview</span>
                     </OptionContainer>
                 </Link>
                 <Link to="/home/rooms" style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <OptionContainer active={location.pathname === '/home/rooms'}>
+                    <OptionContainer active={location.pathname === '/home/rooms'} >
                         <svg xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 7a2 2 0 0 1 2 2m4 0a6 6 0 0 1-7.743 5.743L11 17H9v2H7v2H4a1 1 0 0 1-1-1v-2.586a1 1 0 0 1 .293-.707l5.964-5.964A6 6 0 1 1 21 9" /></svg>
+                        <span className="hover-text">rooms</span>
                     </OptionContainer>
                 </Link>
                 <Link to="/home/bookings" style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <OptionContainer active={location.pathname === '/home/bookings'}>
+                    <OptionContainer active={location.pathname === '/home/bookings'} >
                         <svg xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" viewBox="0 0 15 15"><path fill="none" stroke="currentColor" d="M3.5 0v5m8-5v5M3 7.5h3m6 0H9m-6 3h3m3 0h3m-10.5-8h12a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-12a1 1 0 0 1-1-1v-10a1 1 0 0 1 1-1Z" /></svg>
+                        <span className="hover-text">Bookings</span>
                     </OptionContainer>
                 </Link>
                 <Link to="/home/users" style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <OptionContainer active={location.pathname === '/home/users'}>
+                    <OptionContainer active={location.pathname === '/home/users'} >
                         <svg xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" /><circle cx="12" cy="7" r="4" /></g></svg>
+                        <span className="hover-text">users</span>
                     </OptionContainer>
                 </Link>
                 <Link to="/home/contacts" style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <OptionContainer active={location.pathname === '/home/contacts'}>
+                    <OptionContainer active={location.pathname === '/home/contacts'} >
                         <svg xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M18.118 14.702L14 15.5c-2.782-1.396-4.5-3-5.5-5.5l.77-4.13L7.815 2H4.064c-1.128 0-2.016.932-1.847 2.047c.42 2.783 1.66 7.83 5.283 11.453c3.805 3.805 9.286 5.456 12.302 6.113c1.165.253 2.198-.655 2.198-1.848v-3.584z" /></svg>
+                        <span className="hover-text">contacts</span>
                     </OptionContainer>
                 </Link>
             </OptionList>
--- src/styles/Dashboard/Slidebar.styles.ts
+++ src/styles/Dashboard/Slidebar.styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const SlideContent = styled.div`
   display: flex;
@@ -9,8 +9,7 @@ export const SlideContent = styled.div`
   padding: 1em; 
   padding-left: 0;
   color: grey;
-  width: 100%;
-  max-width: 150px;
+  width: 150px;
 `
 
 export const LogoContainer = styled.div`
@@ -21,6 +20,17 @@ padding:1.2em 1.5em;
 border-radius: 20px;
 `
 
+const slideIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
 interface OptionContainerProp {
   active: boolean
 }
@@ -30,19 +40,46 @@ export const OptionList = styled.div`
   flex-direction: column; 
   align-items: center;
   width: 100%;
+
 `
 
 export const OptionContainer = styled.div<OptionContainerProp>`
 cursor: pointer;
 width: 8em;
-max-width: 8em;
 padding: 1.5em 0;
 text-align: center;
 border-left: ${({ active }) => active ? '5px solid white' : ''};
 color: ${({ active }) => active ? 'white' : 'grey'};
+position: relative;
 
 &:hover{
     color: white;
+    }
+
+    .hover-text {
+        position: absolute;
+        top: -7px;
+        left: 70%;
+        opacity: 0;
+        background-color: rgba(255, 255, 255, 0.9);
+        color: white;
+        background-color: black;
+        padding: 2em 1em;
+        white-space: nowrap;
+        border-radius: 10px;
+        font-size: 1em;
+        text-transform: uppercase;
+        letter-spacing: 0.2em;
+        font-weight: bold;
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+        transform: translateX(10px);
+        width: 9em;
+        text-align: center;
+        z-index: 2;
+    }
+
+    &:hover .hover-text {
+        animation: ${slideIn} 0.3s ease-in-out forwards;
     }
 `
 
